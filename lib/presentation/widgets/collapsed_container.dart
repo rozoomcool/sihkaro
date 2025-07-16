4f08346e2451fa6486b9f7971190350d38a1210c
rozoomcool <rozoom.um@gmail.com>
2025-07-16 18:11:43 +0300

    change animation princip

--- lib/presentation/widgets/collapsed_container.dart
+++ lib/presentation/widgets/collapsed_container.dart
@@ -7,13 +7,13 @@ class CollapsedContainer extends StatefulWidget {
     required this.axis,
     required this.builder,
     required this.alignment,
-    this.size,
+    required this.size,
     this.shape,
     this.color,
     this.padding,
   });
 
-  final double? size;
+  final double size;
   final double collapsedSize;
   final Axis axis;
   final AlignmentGeometry alignment;
@@ -31,51 +31,91 @@ class CollapsedContainer extends StatefulWidget {
   State<CollapsedContainer> createState() => _CollapsedContainerState();
 }
 
-class _CollapsedContainerState extends State<CollapsedContainer> {
-  bool isExtended = true;
-  bool isShowExpanded = true;
+class _CollapsedContainerState extends State<CollapsedContainer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _sizeAnimation;
+  bool isExtended = false;
+  bool isShowExpanded = false;
 
-  void toggle() => setState(() {
-    isExtended = !isExtended;
-    if (!isExtended) {
-      showExpanded(false);
-    }
-  });
+  @override
+  void initState() {
+    super.initState();
 
-  void showExpanded(bool value) {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+
+    _initAnimation();
+  }
+
+  void _initAnimation() {
+    final double begin = widget.collapsedSize;
+    final double end = widget.size;
+
+    _sizeAnimation =
+        Tween<double>(begin: begin, end: end).animate(
+          CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+        )..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+            setState(() => isShowExpanded = true);
+          } else if (status == AnimationStatus.reverse) {
+            setState(() => isShowExpanded = false);
+          }
+        });
+
+    // toggle();
+  }
+
+  void toggle() {
     setState(() {
-      isShowExpanded = value;
+      isExtended = !isExtended;
+      if (isExtended) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
     });
   }
 
   @override
+  void didUpdateWidget(CollapsedContainer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _initAnimation(); // переинициализируем tween при изменениях
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      onEnd: () {
-        if (isExtended) {
-          showExpanded(true);
-        }
+    return AnimatedBuilder(
+      animation: _sizeAnimation,
+      builder: (context, child) {
+        return Align(
+          alignment: widget.alignment,
+          child: Container(
+            decoration: ShapeDecoration(
+              color:
+                  widget.color ??
+                  Theme.of(context).colorScheme.surfaceContainerLow,
+              shape:
+                  widget.shape ??
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+            ),
+            width: widget.axis == Axis.horizontal ? _sizeAnimation.value : null,
+            height: widget.axis == Axis.vertical ? _sizeAnimation.value : null,
+            padding: widget.padding,
+            child: widget.builder(context, isShowExpanded, toggle),
+          ),
+        );
       },
-      alignment: widget.alignment,
-      curve: Curves.easeIn,
-      duration: Duration(milliseconds: 250),
-      decoration: BoxDecoration(
-        color:
-            widget.color ?? Theme.of(context).colorScheme.surfaceContainerLow,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      width: widget.axis == Axis.horizontal
-          ? isExtended
-                ? widget.size
-                : widget.collapsedSize
-          : null,
-      height: widget.axis == Axis.vertical
-          ? isExtended
-                ? widget.size
-                : widget.collapsedSize
-          : null,
-      padding: widget.padding,
-      child: widget.builder(context, isShowExpanded, toggle),
     );
   }
 }
