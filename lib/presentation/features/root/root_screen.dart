496080e2d90338beb4615f2986820eeafbb97b52
rozoomcool <rozoom.um@gmail.com>
2025-07-16 18:11:02 +0300

    set layout

--- lib/presentation/features/root/root_screen.dart
+++ lib/presentation/features/root/root_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sihkaro/presentation/features/chat/chat_panel.dart';
 import 'package:sihkaro/presentation/features/source/source_panel.dart';
 import 'package:sihkaro/presentation/features/studio/studio_panel.dart';
 import 'package:sihkaro/presentation/state/theme_mode_state_widget.dart';
-import 'package:sihkaro/presentation/widgets/collapsed_container.dart';
 import 'package:sihkaro/presentation/widgets/side_bar.dart';
 
 class RootLayout extends StatefulWidget {
@@ -20,8 +20,14 @@ class _RootLayoutState extends State<RootLayout> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
-        title: Text("SihKaro"),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Text("Sihkaro"),
+        ),
         centerTitle: false,
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        notificationPredicate: (_) => false,
         leading: IconButton(
           onPressed: () {
             setState(() {
@@ -53,53 +59,7 @@ class _RootLayoutState extends State<RootLayout> {
                 spacing: 12,
                 children: [
                   SourcePanel(),
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: CollapsedContainer(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadiusGeometry.circular(12),
-                      ),
-                      collapsedSize: 48,
-                      axis: Axis.horizontal,
-                      alignment: Alignment.topLeft,
-                      builder: (context, _, __) {
-                        return Column(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    top: 8.0,
-                                    left: 8.0,
-                                    right: 8.0,
-                                  ),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        "Чат",
-                                        style: Theme.of(
-                                          context,
-                                        ).textTheme.titleMedium,
-                                      ),
-                                      IconButton(
-                                        onPressed: null,
-                                        icon: Icon(Icons.chat),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Divider(),
-                              ],
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
+                  Flexible(fit: FlexFit.tight, child: ChatPanel()),
                   StudioPanel(),
                 ],
               ),
