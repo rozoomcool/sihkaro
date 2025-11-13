5fd276c31cd8dd7bbb9ca3529abacf7612f8912e
rozoomcool <rozoom.um@gmail.com>
2025-11-14 01:22:12 +0300

    minor changes

--- lib/presentation/features/root/root_screen.dart
+++ lib/presentation/features/root/root_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 // import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sihkaro/presentation/router/router.gr.dart';
 import 'package:sihkaro/presentation/state/auth/auth.dart';
 import 'package:sihkaro/presentation/widgets/custom_button_navigation_bar.dart';
 
@@ -17,108 +16,100 @@ class RootScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // final extendedSideBar = useState(false);
     // final authAsync = ref.watch(authProvider);
-    final authNotifier = ref.read(authProvider.notifier);
+    // final authNotifier = ref.read(authProvider.notifier);
 
     return LayoutBuilder(
       builder: (context, constraints) => ConstrainedBox(
         constraints: BoxConstraints(maxWidth: 900),
-        child: AutoTabsRouter.pageView(
-          routes: const [HomeRoute(), HomeRoute(), HomeRoute()],
-          builder: (context, child, controller) {
-            final tabsRouter = AutoTabsRouter.of(context);
-
-            return Scaffold(
-              body: 
-              // constraints.maxWidth < 640?
-              child
-                  // : Flexible(
-                  //     child: Row(
-                  //       mainAxisSize: MainAxisSize.max,
-                  //       children: [
-                  //         NavigationRail(
-                  //           labelType: NavigationRailLabelType.selected,
-                  //           selectedIndex: tabsRouter.activeIndex,
-                  //           onDestinationSelected: tabsRouter.setActiveIndex,
-                  //           destinations: const [
-                  //             NavigationRailDestination(
-                  //               icon: Icon(Icons.home_outlined),
-                  //               selectedIcon: Icon(Icons.home),
-                  //               label: Text('Home'),
-                  //             ),
-                  //             NavigationRailDestination(
-                  //               icon: Icon(Icons.calendar_month_outlined),
-                  //               selectedIcon: Icon(Icons.calendar_month),
-                  //               label: Text('Calendar'),
-                  //             ),
-                  //             NavigationRailDestination(
-                  //               icon: Icon(Icons.email_outlined),
-                  //               selectedIcon: Icon(Icons.email),
-                  //               label: Text('Email'),
-                  //             ),
-                  //           ],
-                  //         ),
-                  //       ],
-                  //     ),
-                  //   ),
-                  ,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                flexibleSpace: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(0),
-                    bottomRight: Radius.circular(0),
-                  ),
-                  child: BackdropFilter(
-                    blendMode: BlendMode.src,
-                    filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(0),
-                          bottomRight: Radius.circular(0),
-                        ),
-                        border: BoxBorder.fromLTRB(
-                          bottom: BorderSide(width: 1, color: Colors.white10),
-                        ),
-                        color: Colors.black.withAlpha(70),
+        child: AutoTabsScaffold(
+          // constraints.maxWidth < 640?
+          // : Flexible(
+          //     child: Row(
+          //       mainAxisSize: MainAxisSize.max,
+          //       children: [
+          //         NavigationRail(
+          //           labelType: NavigationRailLabelType.selected,
+          //           selectedIndex: tabsRouter.activeIndex,
+          //           onDestinationSelected: tabsRouter.setActiveIndex,
+          //           destinations: const [
+          //             NavigationRailDestination(
+          //               icon: Icon(Icons.home_outlined),
+          //               selectedIcon: Icon(Icons.home),
+          //               label: Text('Home'),
+          //             ),
+          //             NavigationRailDestination(
+          //               icon: Icon(Icons.calendar_month_outlined),
+          //               selectedIcon: Icon(Icons.calendar_month),
+          //               label: Text('Calendar'),
+          //             ),
+          //             NavigationRailDestination(
+          //               icon: Icon(Icons.email_outlined),
+          //               selectedIcon: Icon(Icons.email),
+          //               label: Text('Email'),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          appBarBuilder: (context, router) {
+            return AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              flexibleSpace: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                ),
+                child: BackdropFilter(
+                  blendMode: BlendMode.src,
+                  filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(0),
                       ),
+                      border: BoxBorder.fromLTRB(
+                        bottom: BorderSide(width: 1, color: Colors.white10),
+                      ),
+                      color: Colors.black.withAlpha(70),
                     ),
                   ),
                 ),
-                leading: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CircleAvatar(),
+              ),
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(),
+              ),
+              actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+              title: SvgPicture.asset("assets/white1.svg", width: 30),
+            );
+          },
+          backgroundColor: Colors.black,
+          extendBodyBehindAppBar: true,
+          extendBody: true,
+          bottomNavigationBuilder: (context, tabsRouter) {
+            return CustomBottomNavigationBar(
+              selectedIndex: tabsRouter.activeIndex,
+              onTap: tabsRouter.setActiveIndex,
+              items: [
+                CustomBottomNavigationBarItem(
+                  label: 'Statistics',
+                  selectedIcon: Icons.home_filled,
+                  defaultIcon: Icons.home_filled,
                 ),
-                actions: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
-                ],
-                title: SvgPicture.asset("assets/white1.svg", width: 30),
-              ),
-              backgroundColor: Colors.black,
-              extendBodyBehindAppBar: true,
-              extendBody: true,
-              bottomNavigationBar: CustomBottomNavigationBar(
-                selectedIndex: tabsRouter.activeIndex,
-                onTap: tabsRouter.setActiveIndex,
-                items: [
-                  CustomBottomNavigationBarItem(
-                    label: 'Statistics',
-                    selectedIcon: Icons.home_filled,
-                    defaultIcon: Icons.home_filled,
-                  ),
-                  CustomBottomNavigationBarItem(
-                    label: 'Home',
-                    selectedIcon: Icons.chat_rounded,
-                    defaultIcon: Icons.chat_rounded,
-                  ),
-                  CustomBottomNavigationBarItem(
-                    label: 'Settings',
-                    selectedIcon: Icons.settings_rounded,
-                    defaultIcon: Icons.settings_rounded,
-                  ),
-                ],
-              ),
+                CustomBottomNavigationBarItem(
+                  label: 'Home',
+                  selectedIcon: Icons.chat_rounded,
+                  defaultIcon: Icons.chat_rounded,
+                ),
+                CustomBottomNavigationBarItem(
+                  label: 'Settings',
+                  selectedIcon: Icons.settings_rounded,
+                  defaultIcon: Icons.settings_rounded,
+                ),
+              ],
             );
           },
         ),
